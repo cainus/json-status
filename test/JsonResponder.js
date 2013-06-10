bdd80c7afc6c7d7ff574974e5eb84f8d0ca786f8
cainus <gregg@caines.ca>
2013-06-09 22:05:24 -0700

    add error-handling capabilities.

--- test/JsonResponder.js
+++ test/JsonResponder.js
@@ -1,5 +1,7 @@
 var should = require('should');
-var JsonResponder = require('../index');
+var connect = require('connect');
+var request = require('request');
+var JsonStatus = require('../index');
 
 var FakeResponse = function(){
   this.status = null;
@@ -18,12 +20,12 @@ var FakeResponse = function(){
   };
 };
 
-describe("JsonResponder", function(){
+describe("JsonStatus", function(){
 
   describe("#internalServerError", function(){
     it ("sets the status to 500 when the detail is a simple object", function(done){
       var fakeRes = new FakeResponse();
-      var responder = new JsonResponder({}, fakeRes);
+      var responder = new JsonStatus({}, fakeRes);
       var expected = {type : 500,
                       message : 'Internal Server Error',
                       detail : {}};
@@ -42,7 +44,7 @@ describe("JsonResponder", function(){
     });
     it ("sets the status to 500 when the detail is a 'circular' object", function(done){
       var fakeRes = new FakeResponse();
-      var responder = new JsonResponder({}, fakeRes);
+      var responder = new JsonStatus({}, fakeRes);
       var circular = {};
       circular.circular = circular;  // this is circular, in case that's not obvious
       var expected = {type : 500,
@@ -62,10 +64,43 @@ describe("JsonResponder", function(){
       response.error.should.eql(expected);
     });
   });
+    it ("emits error events in the case of errors", function(done){
+      var server = connect.createServer();
+      var responseSent = false;
+      var errorEventSent = false;
+      var middleware = JsonStatus.connectMiddleware('status', function(data){
+        should.exist(data.req);
+        should.exist(data.res);
+        data.type.should.equal(500);
+        data.message.should.equal('Internal Server Error');
+        data.detail.should.equal('some error!');
+        if (responseSent){
+          return done();
+        }
+        errorEventSent = true;
+      });
+      server.use(middleware);
+      server.use(function(req, res){
+        res.status.internalServerError("some error!");
+      });
+      server.listen(8082, function(){
+        request('http://localhost:8082/', function(err, res, body){
+          res.statusCode.should.equal(500);
+          JSON.parse(body).should.eql(
+            {"error":{"type":500,
+                      "message":"Internal Server Error",
+                      "detail":"some error!"}});
+          if (errorEventSent){
+            return done();
+          }
+          responseSent = true;
+        });
+      });
+    });
   describe("#badRequest", function(){
     it ("sets the status to 400", function(done){
       var fakeRes = new FakeResponse();
-      var responder = new JsonResponder({}, fakeRes);
+      var responder = new JsonStatus({}, fakeRes);
       var expected = {type : 400,
                       message : 'Bad Request',
                       detail : 'bad request'};
@@ -86,7 +121,7 @@ describe("JsonResponder", function(){
   describe("#accepted", function(){
     it ("sets the status to 202", function(){
       var fakeRes = new FakeResponse();
-      var responder = new JsonResponder({}, fakeRes);
+      var responder = new JsonStatus({}, fakeRes);
       responder.accepted();
       fakeRes.status.should.equal(202);
       fakeRes.ended.should.equal(true);
@@ -95,7 +130,7 @@ describe("JsonResponder", function(){
   describe("#noContent", function(){
     it ("sets the status to 204", function(){
       var fakeRes = new FakeResponse();
-      var responder = new JsonResponder({}, fakeRes);
+      var responder = new JsonStatus({}, fakeRes);
       responder.noContent();
       fakeRes.status.should.equal(204);
       fakeRes.ended.should.equal(true);
@@ -104,7 +139,7 @@ describe("JsonResponder", function(){
   describe("#resetContent", function(){
     it ("sets the status to 205", function(){
       var fakeRes = new FakeResponse();
-      var responder = new JsonResponder({}, fakeRes);
+      var responder = new JsonStatus({}, fakeRes);
       responder.resetContent();
       fakeRes.status.should.equal(205);
       fakeRes.ended.should.equal(true);
@@ -113,7 +148,7 @@ describe("JsonResponder", function(){
   describe("#movedPermanently", function(){
     it ("sets the Location header and sets the status to 301", function(){
       var fakeRes = new FakeResponse();
-      var responder = new JsonResponder({}, fakeRes);
+      var responder = new JsonStatus({}, fakeRes);
       responder.movedPermanently("SOMEURL");
       fakeRes.headers.Location.should.equal("SOMEURL");
       fakeRes.status.should.equal(301);
@@ -123,7 +158,7 @@ describe("JsonResponder", function(){
   describe("#OPTIONS", function(){
     it ("sets the Allow header and sets the status to 200", function(){
       var fakeRes = new FakeResponse();
-      var responder = new JsonResponder({}, fakeRes);
+      var responder = new JsonStatus({}, fakeRes);
       responder.OPTIONS(["GET", "POST"]);
       fakeRes.headers.Allow.should.equal("GET,POST");
       fakeRes.status.should.equal(200);
@@ -134,7 +169,7 @@ describe("JsonResponder", function(){
   describe("#redirect", function(){
     it ("sets the Location header and sets the status to 301", function(){
       var fakeRes = new FakeResponse();
-      var responder = new JsonResponder({}, fakeRes);
+      var responder = new JsonStatus({}, fakeRes);
       responder.movedPermanently("SOMEURL");
       fakeRes.headers.Location.should.equal("SOMEURL");
       fakeRes.status.should.equal(301);
@@ -144,11 +179,42 @@ describe("JsonResponder", function(){
   describe("#created", function(){
     it ("sets the Location header and sets the status to 201", function(){
       var fakeRes = new FakeResponse();
-      var responder = new JsonResponder({}, fakeRes);
+      var responder = new JsonStatus({}, fakeRes);
       responder.created("SOMEURL");
       fakeRes.headers.Location.should.equal("SOMEURL");
       fakeRes.status.should.equal(201);
       fakeRes.ended.should.equal(true);
     });
+  });
+  describe("connectMiddleware", function(){
+    it ("adds a 'status' object to the response object", function(done){
+      var server = connect.createServer();
+      server.use(JsonStatus.connectMiddleware());
+      server.use(function(req, res){
+        res.status.accepted();
+      });
+      server.listen(8080, function(){
+        request('http://localhost:8080/', function(err, res, body){
+          res.statusCode.should.equal(202);
+          body.should.equal('');
+          done();
+        });
+      });
+    });
+    it ("allows you to set the name of the 'status' object", function(done){
+      var server = connect.createServer();
+      server.use(JsonStatus.connectMiddleware("ohyeah"));
+      server.use(function(req, res){
+        res.ohyeah.accepted();
+      });
+      server.listen(8081, function(){
+        request('http://localhost:8081/', function(err, res, body){
+          res.statusCode.should.equal(202);
+          body.should.equal('');
+          done();
+        });
+      });
+    });
+
   });
 });
